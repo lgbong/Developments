71d32971d9d521607bc13fc9715d445334134a03
Albertoicix <Albertoicix@gmail.com>
2022-07-20 17:14:27 +0900

    lgbong java update two

--- HelloJava.java
+++ HelloJava.java
@@ -3,6 +3,21 @@
  */
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello my first java");
     }
 }
+
+/*
+git init
+git commit -m "lgbong java update"
+git branch -M 'main'
+git remote add origin https://github.com/lgbong/Developments.git
+git push -u origin 'main'
+
+ls
+git commit -m "lgbong java update"
+git branch -M 'main'
+git remote add origin https://github.com/lgbong/Developments.git
+git remote -v
+git push origin main
+*/
